48bdb3778b96a035bd9b98b0a7cc4f77d49ef6ba
rekrutik <rekrutik@gmail.com>
2022-05-18 03:06:20 +0300

    Add dependency of child

--- src/bin/main.dart
+++ src/bin/main.dart
@@ -1,10 +1,24 @@
 import 'package:grpc/grpc.dart';
+import 'package:grpc/grpc_or_grpcweb.dart';
 import 'package:service/chillgen/father_service/public/father.pbgrpc.dart';
+import 'package:service/chillgen/son_service/public/son.pbgrpc.dart';
+import 'dart:io';
 
 class FatherService extends FatherServiceBase {
+
+  late SonServiceClient _client;
+
+  FatherService() {
+    _client = SonServiceClient(GrpcOrGrpcWebClientChannel.grpc(
+        Platform.environment["CHILL_SERVICE_SON_SERVICE"]!,
+        port: 80,
+    ));
+  }
+
   @override
   Future<FatherHelloResponse> sayHello(ServiceCall call, FatherHelloRequest request) async {
-    return FatherHelloResponse()..response = "are ya winning son?";
+    final res = await _client.sayHello(SonHelloRequest());
+    return FatherHelloResponse()..response = "- are ya winning son? - ${res.response}";
   }
 }
 
